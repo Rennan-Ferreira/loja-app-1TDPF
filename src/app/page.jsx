2af78d7a0f012667572580f0ef7aa33e3895a054
Rennan-Ferreira <rm99364@fiap.com.br>
2023-10-18 11:37:11 -0300

    b

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,12 +6,6 @@ export default function Home() {
       <h1>HOME</h1>
       <div>
         <h2>Meus Produtos</h2>
-        <ul>
-          <li><Link href="/produtos/tenis" >Tênis</Link></li>
-          <li><Link href="/produtos/camisa" >Camisa</Link></li>
-          <li><Link href="/produtos/meia" >Meia</Link></li>
-          <li><Link href="/produtos/calca" >Calça</Link></li>
-        </ul>
       </div>
 
     </>
